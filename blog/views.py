af9d0825b5cf52f1ed7d4cc7053347d90031dbb4
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-25 11:48:01 -0300

    Django Template tests

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from blog.data import posts
 
 # Create your views here.
 def blog(request):
-    return render(request, "blog/index.html")
+
+    context = {
+        'text': 'Olá Blog',
+        'posts': posts
+    }
+    return render(request, "blog/index.html", context)
